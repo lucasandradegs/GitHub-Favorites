355e28d4efdc1db8e02df811b8d5572c0be0392d
Lucas Andrade <contato.developerlucas@gmail.com>
2023-08-20 22:15:38 -0300

    using gitHub API to get new users. Saving add/remove operators on LocalStorage

--- js/favorites.js
+++ js/favorites.js
@@ -1,29 +1,65 @@
+export class GitHubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`
+
+        return fetch(endpoint).then(data => data.json()).then(({ login, name, public_repos, followers }) => ({
+            login,
+            name,
+            public_repos,
+            followers
+        }))
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+        GitHubUser.search('lucasandradegs').then(user => console.log(user))
     }
 
     load() {
-        this.entries = [{
-            login: 'lucasandradegs',
-            name: "Lucas Andrade",
-            public_repos: '2',
-            followers: '1432',
-        },
-        {
-            login: 'rui-augusto',
-            name: "Rui Augusto",
-            public_repos: '8',
-            followers: '8271',
-        }  
-        ]
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+
+            if(userExists) {
+                throw new Error('Usuário já cadastrado')
+            }
+
+
+
+            const user = await GitHubUser.search(username)
+            
+            if(user.login === undefined) {
+                throw new Error('Usuário não econtrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch (err) {
+            alert(err.message)
+        }       
     }
 
     delete(user) {
         const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
 
-        console.log(filteredEntries)
+        this.entries = filteredEntries
+        this.update()
+        this.save()
     }
 }
 
@@ -35,6 +71,16 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
 
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
